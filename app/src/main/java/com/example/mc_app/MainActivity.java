7324e164f29abb227a51e2f13f67d1976bba05da
Usama Ijaz <usamaijaz2001@gmail.com>
2022-05-22 12:31:09 +0500

    Web Browser example.

--- app/src/main/java/com/example/mc_app/MainActivity.java
+++ app/src/main/java/com/example/mc_app/MainActivity.java
@@ -11,16 +11,26 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button btn;
+    Button btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn=findViewById((R.id.btn));
+        btn1=findViewById((R.id.btn1));
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Uri uri=Uri.parse("tel:+923339448548");
                 Intent intent=new Intent(Intent.ACTION_DIAL,uri);
+                startActivity(intent);
+            }
+        });
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri=Uri.parse("http://www.google.com/");
+                Intent intent=new Intent(Intent.ACTION_VIEW,uri);
                 startActivity(intent);
             }
         });
