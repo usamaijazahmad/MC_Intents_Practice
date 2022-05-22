15709ac6f79d2ecea2ea525856bcc46de990f733
Usama Ijaz <usamaijaz2001@gmail.com>
2022-05-22 13:24:14 +0500

    Compose Email example.

--- app/src/main/java/com/example/mc_app/MainActivity.java
+++ app/src/main/java/com/example/mc_app/MainActivity.java
@@ -12,12 +12,14 @@ public class MainActivity extends AppCompatActivity {
 
     Button btn;
     Button btn1;
+    Button btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn=findViewById((R.id.btn));
-        btn1=findViewById((R.id.btn1));
+        btn=findViewById(R.id.btn);
+        btn1=findViewById(R.id.btn1);
+        btn2=findViewById(R.id.btn2);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -32,6 +34,22 @@ public class MainActivity extends AppCompatActivity {
                 Uri uri=Uri.parse("http://www.google.com/");
                 Intent intent=new Intent(Intent.ACTION_VIEW,uri);
                 startActivity(intent);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String address="usamaijaz2001@gmail.com";
+                String subject="Mobile Computing";
+                Uri uri=Uri.parse("mailto:");
+                Intent intent=new Intent(Intent.ACTION_SEND,uri);
+                intent.setData(uri);
+                intent.setType("*/*");
+                intent.putExtra(Intent.EXTRA_EMAIL,new String[]{address});
+                intent.putExtra(Intent.EXTRA_SUBJECT,subject);
+                if(intent.resolveActivity(getPackageManager())!=null) {
+                    startActivity(intent);
+                }
             }
         });
     }
